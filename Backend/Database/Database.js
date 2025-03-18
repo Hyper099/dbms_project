4dd155257401adce411398eca06029cb60df18ae
Jayneel Mahival <jm.mahival@gmail.com>
2025-03-18 23:07:59 +0530

    added  new database cloud for mysql

--- Backend/Database/Database.js
+++ Backend/Database/Database.js
@@ -4,10 +4,16 @@ const mysql = require('mysql2');
 
 // Create connection to the mysql database
 const db = mysql.createConnection({
-   host: process.env.MYSQLHOST,
-   user: process.env.MYSQLUSER,
-   password: process.env.MYSQLPASSWORD,
-   database: 'dbms_project'
+   host: process.env.MYSQL_ADDON_HOST,
+   port: process.env.MYSQL_ADDON_PORT,
+   user: process.env.MYSQL_ADDON_USER,
+   password: process.env.MYSQL_ADDON_PASSWORD,
+   database: process.env.MYSQL_ADDON_DB
+
+   // host: process.env.MYSQLHOST,
+   // user: process.env.MYSQLUSER,
+   // password: process.env.MYSQLPASSWORD,
+   // database: 'dbms_project'
 });
 
 db.connect((err) => {
@@ -15,7 +21,7 @@ db.connect((err) => {
       console.error('Error connecting to MySQL:', err);
       process.exit(1);
    }
-   console.log(`Connected to MySQL Database`);
+   console.log(`✅ Connected to MySQL Database `);
 });
 
 module.exports = db;
